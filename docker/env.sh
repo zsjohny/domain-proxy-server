dc5d7f20c9d33138d2ec0205271f68ac0092249a
Johny.Zheng <shun.johny@gmail.com>
2017-11-07 20:05:04 +0800

    no message

--- docker/env.sh
+++ docker/env.sh
@@ -2,7 +2,7 @@
 # -*- encoding UTF-8 -*-
 # Author: Johny
 
-export DOCKER_REGISTRY_URL=registry.kuick.cn/cc
+export DOCKER_REGISTRY_URL=zsjohny
 export DOCKER_IMAGE_NAME=domain-proxy-server
 export DOCKER_IMAGE_NAME_TESTER=domain-proxy-tester
 export DOCKER_INSTANCE_NAME=domainproxyserver
