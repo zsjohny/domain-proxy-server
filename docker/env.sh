ce3e7fe649b4de084fa565a84c4942342fac9a90
Johny.Zheng <shun.johny@gmail.com>
2017-11-07 20:01:14 +0800

    80

--- docker/env.sh
+++ docker/env.sh
@@ -6,4 +6,4 @@ export DOCKER_REGISTRY_URL=registry.kuick.cn/cc
 export DOCKER_IMAGE_NAME=domain-proxy-server
 export DOCKER_IMAGE_NAME_TESTER=domain-proxy-tester
 export DOCKER_INSTANCE_NAME=domainproxyserver
-export DOCKER_INSTANCE_PORT=38081
+export DOCKER_INSTANCE_PORT=80
