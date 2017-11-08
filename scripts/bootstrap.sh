0e28937490a58b85ccbcf16803585997748917f2
Johny.Zheng <shun.johny@gmail.com>
2017-11-08 12:50:48 +0800

    fix

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,7 +11,7 @@ sleep 5s
 
 if test `docker  --version| grep -c 17` -eq 1
 then
-    echo  '您已经安装最新版本docker', $DOCKER_VERISON
+    echo '您已经安装最新版本docker', $DOCKER_VERISON
 else
     echo '您需要安装最新版本docker,请运行 update_docker_from_offcial.sh'
     exit
@@ -19,7 +19,7 @@ fi
 
 if test `docker-compose --version| grep -c 1.17.0` -eq 1
 then
-    echo  '您已经安装最新版本docker-compose', $COMPOSE_VERISON
+    echo '您已经安装最新版本docker-compose', $COMPOSE_VERISON
 else
     echo '您需要安装最新版本docker-compose,请运行update_docker-compose.sh'
     sh update_docker-compose.sh
@@ -27,13 +27,13 @@ fi
 
 
 # Prepare Image
-echo -e "\033[33m 正在准备二级域名代理服务...... \033[0m"
+echo -e "\033[33m正在准备二级域名代理服务......\033[0m"
 
 ../docker/deploy.sh
 
 if [ $? -eq 0 ]
 then
-echo -e "\033[32m 部署成功！ \033[0m";
+echo -e "\033[32m部署成功！ \033[0m";
 else
-echo -e "\033[31m 部署失败,请检查本地网络及代理设置。 \033[0m"
+echo -e "\033[31m部署失败,请检查本地网络及代理设置。\033[0m"
 fi
