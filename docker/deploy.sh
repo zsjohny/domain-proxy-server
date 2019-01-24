97069efadebfb2e754ebbe0861aa8d126e119265
zhoubangjun <zhoubangjun@kuick.cn>
2019-01-24 20:14:05 +0800

    fix: 修复部署

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -17,4 +17,4 @@ echo step1. rm  $DOCKER_INSTANCE_NAME
 docker rm -f $DOCKER_INSTANCE_NAME
 
 echo step2. run $DOCKER_INSTANCE_NAME
-docker run --name=$DOCKER_INSTANCE_NAME --hostname=$DOCKER_INSTANCE_NAME -d --restart=always $DOCKER_OPTS $DOCKER_REGISTRY_URL/$DOCKER_IMAGE_NAME:$VERSION
+docker run --name=$DOCKER_INSTANCE_NAME --hostname=$DOCKER_INSTANCE_NAME -d -v certs:/etc/nginx/certs --restart=always $DOCKER_OPTS $DOCKER_REGISTRY_URL/$DOCKER_IMAGE_NAME:$VERSION
